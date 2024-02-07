f6c17820a10c73f0dd78f61f820ba54f5e8b4300
Parikshit Adhikari <83907047+parikshitadhikari@users.noreply.github.com>
2024-02-07 22:54:15 +0545

    Delete backend directory

--- backend/analysis/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import MyFile
-
-
-class MyFileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MyFile
-        fields = ('file', 'description', 'uploaded_at')
--- backend/analysis/urls.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-urlpatterns = [
-    path('api/upload/', views.MyFileView.as_view(), name='upload' ),
-]
