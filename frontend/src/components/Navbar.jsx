0d7c670a2e68017fbf969cf9398ec4e66e5dd0a4
parikshitadhikari <parikshitadhikari@gmail.com>
2024-02-11 20:29:24 +0545

    homepage styling changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,27 +1,32 @@
 import { Link } from "react-router-dom";
-import Logo from "../../assets/opinix text white.png";
+import Logo from "../../assets/opinix-white.png";
 import "../../styles/components/Navbar.css";
 const Navbar = () => {
   return (
     <>
-      <div className="navMainDiv">
-        <Link to="/">
-          <div>
-            <img src={Logo} alt="Opinix" className="logo" />
-          </div>
-        </Link>
-        <div className="navBottomDiv">
-          <Link to="/analyze" className="customLink">
-            Analyze
-          </Link>
-          <Link to="/history" className="customLink">
-            History
-          </Link>
-          <Link to="/about" className="customLink">
-            About
+      <nav className="nav">
+        <div className="nav-left-section">
+          <Link to="/">
+            <div>
+              <img className="logo" src={Logo} alt="Logo" />
+            </div>
           </Link>
         </div>
-      </div>
+        <div className="nav-right-section">
+          <div className="nav-links">
+            <Link className="nav-link" to="/history">
+              History
+            </Link>
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
+          </div>
+          <Link to="/analyze">
+            <button className="nav-button">Analyze</button>
+          </Link>
+          {/*<img src="profile.png" alt="Profile" className="profile" /> */}
+        </div>
+      </nav>
     </>
   );
 };
