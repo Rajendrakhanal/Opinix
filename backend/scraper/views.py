b43fa6c22d644f453d87bfa71660ae9764006a84
Rajendrakhanal <khanal.rajendra111@gmail.com>
2024-03-07 12:42:46 +0545

    implemented scraping of reviewTime

--- backend/scraper/views.py
+++ backend/scraper/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from bs4 import BeautifulSoup
 import requests
+import re
+from datetime import datetime
 
 class MyScraperView(APIView):
     def post(self, request, *args, **kwargs):
@@ -20,21 +22,33 @@ class MyScraperView(APIView):
             soup = BeautifulSoup(response.content, 'html.parser')
 
             # Extract reviews and ratings (adjust according to the structure of the webpage)
-            reviewText = []  # Store extracted reviews
+            reviewText = [] # Store extracted reviews
+            dates=[] 
 
             # Example extraction
-            review_elements = soup.find_all(class_='review-text')
-            rating_elements = soup.find_all(class_='review-rating')
+            review_elements = soup.find_all('div',class_='a-expander-content reviewText review-text-content a-expander-partial-collapse-content')
+            date_elements = soup.find_all('span',class_='review-date')
+
 
             for review in review_elements:
                 reviewText.append(review.get_text().strip())
 
 
+            for date in date_elements:
+                dates_text=date.get_text().strip()
+                date_matches = re.findall(r'\b(?:January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2},\s+\d{4}\b', dates_text)
+                dates.append(date_matches)
+
+
+            # Convert date strings to %Y-%m-%d format
+            dates = [datetime.strptime(date[0], "%B %d, %Y").strftime("%Y-%m-%d") for date in dates]
+
             # Prepare JSON response
             response_data = {
                 'message': 'Data scraped successfully',
                 'productLink': product_link,
-                'reviews': reviewText
+                'reviews': reviewText,
+                'dates':dates
             }
 
             # Return JSON response
