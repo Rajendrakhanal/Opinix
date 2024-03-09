fa56c938807293e3b5861061de545f1d2541018e
parikshitadhikari <parikshitadhikari@gmail.com>
2024-03-09 16:44:34 +0545

    homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,20 +1,27 @@
 import { Link } from "react-router-dom";
 import "../../styles/pages/HomePage.css";
+import Background from "../../assets/bg.png";
 const HomePage = () => {
   return (
     <>
-      <div className="banner">
-        <div className="content">
-          <span>Understanding your audience better with sentiment analysis</span>
-          <p>
-            Sentiment Analysis is a powerful tool that can help you understand
-            how people feel about your products, services, or brand. It analyzes
-            text data to determine the sentiment expressed in each piece of
-            content.{" "}
-          </p>
-          <Link to="/analyze">
-            <button className="banner-button">Analyze New Content</button>
-          </Link>
+      <div className="hp-outer-div">
+        <div className="hp-text-div">
+          <span className="hp-heading">Sentiment Analysis</span>
+          <br />
+          <span className="hp-title">
+            Understanding your audience better.
+          </span>
+          <br></br>
+          <br></br>
+          <span className="hp-nocode">No-Code Text Analytics</span>
+          <div className="content">
+            <Link to="/analyze">
+              <button className="hp-button">Analyze New Content</button>
+            </Link>
+          </div>
+        </div>
+        <div className="hp-img-div">
+          <img src={Background}></img>
         </div>
       </div>
     </>
